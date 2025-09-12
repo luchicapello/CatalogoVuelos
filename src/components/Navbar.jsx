cd75637bf7788fc37d73ee2683850fe139e1dfa3
Francisco Castagnaro <64395269+FranciscoCastagnaro@users.noreply.github.com>
2025-09-12 10:53:21 -0300

    Alta de vuelos btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { Plane } from "lucide-react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-export default function Navbar({ current, setCurrent }) {
+export default function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
     <header className="sticky top-0 z-30 backdrop-blur bg-white/70 shadow-md font-sans">
       <div className="max-w-6xl mx-auto px-4 py-3 flex items-center justify-between">
@@ -16,15 +19,13 @@ export default function Navbar({ current, setCurrent }) {
         </div>
         <nav className="flex items-center gap-1 text-sm">
           {[
-            { key: "home", label: "Home" },
-            { key: "login", label: "Login" },
-            { key: "signup", label: "Signup" },
+            { key: "/vuelos/nuevo", label: "Alta de Vuelos" },
           ].map((t) => (
             <button
               key={t.key}
-              onClick={() => setCurrent(t.key)}
+              onClick={() => navigate(t.key)}
               className={`px-3 py-1.5 rounded-xl transition border cursor-pointer ${
-                current === t.key
+                location.pathname === t.key
                   ? "bg-gray-900 text-white border-gray-900"
                   : "hover:bg-primary/20 border-transparent"
               }`}
