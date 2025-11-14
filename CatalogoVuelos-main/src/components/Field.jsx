f519bbb87eb092df5c953c66d8f9b94bc9d15df3
bsalvalai <165174932+bsalvalai@users.noreply.github.com>
2025-11-13 22:56:16 -0300

    Delete CatalogoVuelos-main directory

--- CatalogoVuelos-main/src/components/Field.jsx
+++
@@ -1,8 +0,0 @@
-export default function Field({ label, children }) {
-  return (
-    <label className="flex flex-col gap-1 text-sm">
-      <span className="text-gray-600">{label}</span>
-      {children}
-    </label>
-  );
-}
